16a1eab9bacd43b04c85525d4c57953e4edcfc11
GauravHmm <gauravrajpurohit2006@gmail.com>
2025-03-03 23:25:22 +0530

    added few functions,end of the day

--- emulator.py
+++ emulator.py
@@ -6,4 +6,15 @@ class CPU:
         SP=0x00FF
         C=Z=I=D=B=V=N=0
         A=X=Y=0
-           
+    def Fetch(Cycles,mem):
+        ins=mem[PC]
+        PC+=1
+        Cycles-=1
+        return ins
+    def Execute(mem,Cycles=2):
+        while(Cycles>0):
+            ins=Fetch(Cycles,mem)
+class Memory:
+    def __init__(self):
+        self.mem = [0]*64*1024
+              
