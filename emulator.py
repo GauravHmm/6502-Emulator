bb56052f2fdc5020b9159ee7ef017fa391124161
GauravHmm <gauravrajpurohit2006@gmail.com>
2025-03-05 21:28:29 +0530

    i missed a quiz for this

--- emulator.py
+++ emulator.py
@@ -1,20 +1,40 @@
 class CPU:
-    def __init__(self,PC=0):
-        self.reset(PC)
-    def reset(self,PC=0):
+    def __init__(self):
+        self.reset()
+    def reset(self):
         self.PC=0xFFFC
         self.SP=0x00FF
         self.C=self.Z=self.I=self.D=self.B=self.V=self.N=0
         self.A=self.X=self.Y=0
-    def Fetch(Cycles,mem):
-        ins=mem[PC]
-        PC+=1
-        Cycles-=1
+    def fetch(self,memory):
+        ins=memory.read(self.PC)
+        self.PC+=1
+        self.cycles-=1
         return ins
-    def Execute(self,mem,Cycles=2):
-        while(Cycles>0):
-            self.ins=self.Fetch(Cycles,mem)
+    def execute(self,memory,cycles=2):
+        while(cycles>0):
+            opcode=self.fetch(memory)
+            cycles-=1
+            if opcode==0xA9:
+                self.A=self.fetch(memory)
+                cycles-=1
+            elif opcode==0x00:
+                print("Execution Halted")
+                break
+            else:
+                print(f"Unknown opcode: {hex(opcode)}")
 class Memory:
     def __init__(self):
         self.mem = [0]*64*1024
-              
+    def read(self,address):
+        return self.mem[address]
+    def write(self,address,value):
+        self.mem[address]=value & 0xFF   
+    def loadbin(self,filename,start_address=):
+        with open(filename,'rb') as f:
+            binary_data=f.read()
+            self.mem[start_address:start_address+len(binary_data)]=list(binary_data)
+if __name__=="__main__":
+    memory=Memory()
+    cpu=CPU()
+    cpu.execute(memory)            
