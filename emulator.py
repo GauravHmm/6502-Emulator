a21724098e5e26909ecb7fd0f7b22151e41c1fd0
GauravHmm <gauravrajpurohit2006@gmail.com>
2025-03-05 20:29:59 +0530

    interesting

--- emulator.py
+++ emulator.py
@@ -1,19 +1,19 @@
 class CPU:
-    def __init__(self,pc=0):
-        self.reset(pc)
-    def reset(self,pc=0):
-        PC=0xFFFC
-        SP=0x00FF
-        C=Z=I=D=B=V=N=0
-        A=X=Y=0
+    def __init__(self,PC=0):
+        self.reset(PC)
+    def reset(self,PC=0):
+        self.PC=0xFFFC
+        self.SP=0x00FF
+        self.C=self.Z=self.I=self.D=self.B=self.V=self.N=0
+        self.A=self.X=self.Y=0
     def Fetch(Cycles,mem):
         ins=mem[PC]
         PC+=1
         Cycles-=1
         return ins
-    def Execute(mem,Cycles=2):
+    def Execute(self,mem,Cycles=2):
         while(Cycles>0):
-            ins=Fetch(Cycles,mem)
+            self.ins=self.Fetch(Cycles,mem)
 class Memory:
     def __init__(self):
         self.mem = [0]*64*1024
